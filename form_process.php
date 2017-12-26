7df529353c60566f72090e72b875903d1b428be0
Remco Land <remcoland59@gmail.com>
2017-12-26 15:04:36 +0100

    php

--- form_process.php
+++ form_process.php
@@ -4,8 +4,8 @@ $first_name = $_POST['first_name'];
 $last_name = $_POST['last_name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
-$to = "lemoassistant@gmail.com";
-$subject = "Nieuw message from contact form"
+$to = 'lemoassistant@gmail.com';
+$subject = 'Nieuw message from contact form';
 	
 mail ($to, $subject, $message, "From: " . $first_name . $last_name);
 echo "The message has been sent";
